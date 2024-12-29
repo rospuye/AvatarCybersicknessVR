a470dc4160d9f7f8ac9c433904b102466d2522f0
Isabel Rosário <isabel.rosario102000@gmail.com>
2024-12-29 11:32:13 +0000

    small debug in writing to csv

--- Assets/Project/Scripts/InputFieldHandler.cs
+++ Assets/Project/Scripts/InputFieldHandler.cs
@@ -45,6 +45,6 @@ public class InputFieldHandler : MonoBehaviour
         File.WriteAllText(filePath, csvContent);
 
         Debug.Log("CSV file created: " + filePath);
-        PlayerPrefs.SetString("UserFile",fileName);
+        PlayerPrefs.SetString("UserFile", fileName);
     }
 }
